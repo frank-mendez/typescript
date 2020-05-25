515219d4c89d7754a62587cbe14dc470c6d2a359
Frank Mendez <frankmendezwebdev@gmail.com>
2020-05-25 19:21:56 +0800

    Using Types

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Test Typescript');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var numberOne = '5';
+var numberTwo = 2.8;
+var result = add(numberOne, numberTwo);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('Test Typescript');
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const numberOne = 5;
+const numberTwo = 2.8;
+
+const result = add(numberOne, numberTwo);
+console.log(result);

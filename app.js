e2dbdaf95b73df11bc9e401e34a3b587887b539d
Frank Mendez <frankmendezwebdev@gmail.com>
2020-06-01 21:42:23 +0800

    Working with Enums

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Frank',
+//   age: 30,
+//   hobbies: ['computer games', 'poker'],
+//   role: [2, 'author'],
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Frank',
     age: 30,
-    hobbies: ['computer games', 'poker']
+    hobbies: ['computer games', 'poker'],
+    role: Role.ADMIN
 };
 var favoriteActivites;
 favoriteActivites = ['Dota'];
@@ -9,4 +30,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }

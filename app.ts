01d735850eb79effc85b76744b685cfe1747d26e
frankmendezwebdev <frankmendezwebdev@gmail.com>
2020-06-07 11:14:18 +0800

    Function Return types and voi

--- app.ts
+++ app.ts
@@ -1,31 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 27, 'as-number');
-console.log(combineAges);
+function printResult(num: number) {
+  console.log('result: ', num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combineNames = combine('Frank', 'Rhoie', 'as-text');
-console.log(combineNames);
+printResult(add(5, 2));
